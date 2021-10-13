e3664a57f1df9e88808029a1c1997031cdb901ec
John Villalvazo <jvillalvazo2@ucmerced.edu>
2021-10-12 23:03:07 -0700

    progress towards outputting all students

--- Lab_5/index.js
+++ Lab_5/index.js
@@ -1,54 +1,63 @@
-const xhttp = new XMLHttpRequest();
-// const URL;
 const async = true;
 let fullName = "jhn";
-let lastName;
 let gpa;
-// xhttp.send();
+
+let option = document.querySelectorAll("Action-Options");
+
+// function getValues(){   //! this reads my input fields
+//     fullName = document.getElementById("fullName").value;
+//     // lastName = document.getElementById("lname").value;
+//     gpa = document.getElementById("gpa").value;
+
+//     document.getElementsByClassName("output").innerHTML = fullName;
 
 
-function getValues(){   //! this reads my input fields
-    fullName = document.getElementById("fullName").value;
-    // lastName = document.getElementById("lname").value;
-    gpa = document.getElementById("gpa").value;
-
-    document.getElementsByClassName("output").innerHTML = fullName;
+// }    
+console.log("I am outside")
 
 
-    
-    // console.log(firstName);
+
+
+function getAllStudents() {
+    const xhttp = new XMLHttpRequest
+
+        console.log("omg i was pressed")
+ 
+    xhttp.open("GET", "https://amhep.pythonanywhere.com/grades", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+//     console.log("3");
+    xhttp.onload = function(){
+        // console.log("4");
+        //!will probably need to deciper all of these outputs (using split?) then then output
+        allstudents = this.responseText;
+        document.getElementById('output').innerHTML = allstudents;
+        // console.log(text)
+    };
+//     console.log("5");
+
+    xhttp.send();
+//     console.log("6");
 
 }
 
 
+// function addStudentAJAX() {
+//     console.log("1");
+//     let URL = "https://amhep.pythonanywhere.com/grades";
+//     console.log("2");
+//     let method = "POST";
+//     xhttp.open("POST", "https://amhep.pythonanywhere.com/grades", async);
+//     console.log("3");
 
-function addStudentAJAX() {
-    let option = "Add Student";
-    document.getElementById("optionDisplay").innerText = option;
-    // getValues();
-    // console.log(fullName);
-    console.log(gpa);
-    const URL = "https://amhep.pythonanywhere.com/grades"
-    console.log("I am inside of add student")
-    const method = "POST";
-    xhttp.open(method, URL, async);
-    console.log(fullName);  //! ended here
-    document.getElementsByClassName("output").innerText = fullName;
+//     xhttp.onload = function(){
+//         console.log("4");
+//         document.getElementById("add-item").innerHTML = this.response;
+//     };
+//     console.log("5");
+
+//     xhttp.send();
+//     console.log("6");
+
+// }
 
 
-    
-    
-
-
-    // xhttp.setRequestHeader("Content-Type","application/json");
-    // xhttp.onreadystatechange = function () {
-    //     // document.getElementById('test').innerHTML = this.responseText;
-    //     if(this.readyState == 4 && this.status == 200){
-    //         document.getElementById("output").innerHTML = this.responseText;
-    //     }
-    // };
-
-    // body = {"name":fullName,"grade":gpa};
-    // // xhttp.send(JSON.stringify(body));
-    // xhttp.send(JSON.stringify(body));
-}
