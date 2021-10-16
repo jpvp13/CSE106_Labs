77efbdd172d5e0ad614715aef9882580c6bef449
John Villalvazo <jvillalvazo2@ucmerced.edu>
2021-10-16 15:16:15 -0700

    current work in progress

--- Lab_5/index.js
+++ Lab_5/index.js
@@ -4,60 +4,67 @@ let gpa;
 
 let option = document.querySelectorAll("Action-Options");
 
-// function getValues(){   //! this reads my input fields
-//     fullName = document.getElementById("fullName").value;
-//     // lastName = document.getElementById("lname").value;
-//     gpa = document.getElementById("gpa").value;
-
-//     document.getElementsByClassName("output").innerHTML = fullName;
-
-
-// }    
-console.log("I am outside")
-
+  
 
 
 
 function getAllStudents() {
-    const xhttp = new XMLHttpRequest
-
-        console.log("omg i was pressed")
- 
-    xhttp.open("GET", "https://amhep.pythonanywhere.com/grades", true);
-    xhttp.setRequestHeader("Content-Type", "application/json");
-//     console.log("3");
-    xhttp.onload = function(){
-        // console.log("4");
-        //!will probably need to deciper all of these outputs (using split?) then then output
-        allstudents = this.responseText;
-        document.getElementById('output').innerHTML = allstudents;
-        // console.log(text)
+    clearGetAllStudents();
+    const xhttp = new XMLHttpRequest();
+    document.getElementById('output').style.display = 'block';
+    
+    xhttp.onload = function() {
+        // const myObj =JSON.parse(this.responseText);
+        const myObj = JSON.stringify(JSON.parse(this.responseText), null, 2);   //! this converts my JSON response into a string and changes size
+        let i = 0;
+        for(i; i < myObj.length; i++){  //! this iterates through the length of the JSON response
+            if( myObj[i] != "{" & myObj[i] != "}"  & myObj[i] != '"' & myObj[i] != ","){    //! checks to remove the {}"," to help diplay to user
+                
+                console.log("This is the contents = " + myObj[i]);
+                // addto = myObj[i]
+                document.getElementById("output").innerHTML += myObj[i];    //~ this will append the newly cleaned text to the end of the previously displayed message
+            }
+        }
     };
-//     console.log("5");
 
+    xhttp.open("GET","https://amhep.pythonanywhere.com/grades", true);
     xhttp.send();
-//     console.log("6");
-
 }
 
+function clearGetAllStudents(){
+    document.getElementById("output").innerHTML = "<legend><em><u>Students Name : GPA</u></em></legend> ";
+}
 
-// function addStudentAJAX() {
-//     console.log("1");
-//     let URL = "https://amhep.pythonanywhere.com/grades";
-//     console.log("2");
-//     let method = "POST";
-//     xhttp.open("POST", "https://amhep.pythonanywhere.com/grades", async);
-//     console.log("3");
+function addStudentAJAX(){
+    console.log("I am inside of add student")
 
-//     xhttp.onload = function(){
-//         console.log("4");
-//         document.getElementById("add-item").innerHTML = this.response;
-//     };
-//     console.log("5");
+    let fullName = document.getElementById("fullName").value;
+    let bga = document.getElementById("Gpa").value;
+    // clearContents();
 
-//     xhttp.send();
-//     console.log("6");
+    console.log(fullname);
+    console.log(gpa)
 
-// }
+    document.getElementById("searchBar").style.display = 'block';
+    document.getElementById("output").style.display = 'block';
+}
 
+// function getValues(){   //! this reads my input fields
+//     console.log("I am inside of getValues")
+//     fullName = document.getElementById("fullName").value;
+//     // lastName = document.getElementById("lname").value;
+//     gpa = document.getElementById("gpa").value;
+
+//     console.log("The name inside of fullName is =" + fullName);
+//     console.log("The number inside of gpa is =" + gpa);
+//     // document.getElementsByClassName("output").innerHTML = fullName;
+
+
+// }  
+
+function clearContents() {  //~ clears out the output area ready for new input
+    document.getElementById("searchBar").style.display = "none";
+    document.getElementById("output").style.display = "none";
+    document.getElementById("output").innerHTML = "";
+}
 
